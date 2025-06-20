d8370c702a2607d920f2548700896188690b3330
jam <jaim.tan@student.ateneo.edu>
2025-06-20 18:07:03 +0800

    [Dev] Frontend - Navbar Started

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,9 +5,14 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  templateUrl: './nav.component.html',
+  // template: `
+  //   <h1>Hello world!</h1>
+  // `,
+  // styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  // title = 'frontend';
+  title = 'home';
 }
