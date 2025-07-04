eb069228ee2d5f3c21da7b102947ab03490d5147
jam <jaim.tan@student.ateneo.edu>
2025-07-04 17:42:04 +0800

    [Dev] Frontend - Working Sidebar

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconsComponent} from '../icons/icons.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,16 +11,47 @@ import { RouterModule } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
+
 export class SidebarComponent {
 
-  currentIcon: string = 'nav_home';
   isCollapsed = false;
-  isEnabled: boolean = false;
 
-  // toggleIcon() {
-  //   this.isEnabled = !this.isEnabled;
-  //   this.currentIcon = this.isEnabled ? 'icon-home-enabled' : 'icon-home';
-  // }
+  navItems = [
+    {id: 'home', active: false, title: 'Home', category: 'general'},
+    {id: 'inbox', active: false, title: 'Inbox', category: 'general'},
+    {id: 'calendar', active: false, title: 'Calendar', category: 'general'},
+    {id: 'directory', active: false, title: 'Directory', category: 'general'},
+    {id: 'forms', active: false, title: 'Forms', category: 'general'},
+    {id: 'todo', active: false, title: 'To-do List', category: 'workspace'},
+    {id: 'notes', active: false, title: 'Personal Notes', category: 'workspace'},
+    {id: 'links', active: false, title: 'Client Links', category: 'workspace'},
+  ];
+
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log(this.router.url);
+        
+          for (const item of this.navItems) {
+            if (item.id == this.router.url.replace('/','')) {
+              item.active = true;
+            } else {
+              item.active = false;
+            }
+          }
+      }
+    });
+  }
+
+
+
+  toggleIcon(target:string) {
+    this.router.navigate(['/' + target]);
+  }
+
+  isActive(id: string): boolean {
+    return this.navItems.find(item => item.id === id)?.active || false;
+  }
 
   toggleSidebar() {
     this.isCollapsed = !this.isCollapsed;
