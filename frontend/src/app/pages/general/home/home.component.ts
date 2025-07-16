c07ffad7a031431f2e346154c40bf841e88d142a
Jaim <131182310+jaim-kia@users.noreply.github.com>
2025-07-16 16:27:26 +0800

    [Dev] Frontend - Homepage marquee

--- frontend/src/app/pages/general/home/home.component.ts
+++ frontend/src/app/pages/general/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconsComponent } from '../../../components/icons/icons.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,IconsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  getRange(count: number): number[] {
+    return Array(count).fill(0).map((x, i) => i);
+  }
 
+  getBannerPairs() {
+    const pairWidth = 64; 
+    const screenWidth = window.innerWidth;
+    return Math.ceil(screenWidth / pairWidth) * 2; 
+  }
 }
